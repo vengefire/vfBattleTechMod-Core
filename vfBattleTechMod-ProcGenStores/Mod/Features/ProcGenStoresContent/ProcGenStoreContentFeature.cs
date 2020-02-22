6962bcb746b6ae989b3eba72323158191478c1f4
Stephen Weistra <vengefire@gmail.com>
2020-02-22 21:21:03 +0800

    For some reason, Refresh Shop is called multiple times on start game, and this messes with the inventory. Added a 1 minute cool down check to throttle generation, and problem solved...

--- vfBattleTechMod-ProcGenStores/Mod/Features/ProcGenStoresContent/ProcGenStoreContentFeature.cs
+++ vfBattleTechMod-ProcGenStores/Mod/Features/ProcGenStoresContent/ProcGenStoreContentFeature.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using BattleTech;
+using Harmony;
+using Newtonsoft.Json;
 using vfBattleTechMod_Core.Mods.BaseImpl;
 using vfBattleTechMod_Core.Mods.Interfaces;
 using vfBattleTechMod_ProcGenStores.Mod.Features.ProcGenStoresContent.Logic;
@@ -11,6 +15,7 @@ namespace vfBattleTechMod_ProcGenStores.Mod.Features.ProcGenStoresContent
     public class ProcGenStoreContentFeature : ModFeatureBase<ProcGenStoreContentFeatureSettings>
     {
         private new static ProcGenStoreContentFeature Myself;
+        private DateTime _lastGenDateTime;
 
         public Dictionary<BattleTechResourceType, ShopItemType> dictResourceTypeToShopitemType = new Dictionary<BattleTechResourceType, ShopItemType>
         {
@@ -38,15 +43,41 @@ namespace vfBattleTechMod_ProcGenStores.Mod.Features.ProcGenStoresContent
                     typeof(ProcGenStoreContentFeature).GetMethod("PrefixRefreshShop"),
                     null,
                     null,
-                    0)
+                    0),
+                /*new ModPatchDirective(
+                    typeof(Shop).GetMethod("Initialize"),
+                    typeof(ProcGenStoreContentFeature).GetMethod("PrefixInitializeShop"),
+                    null,
+                    null,
+                    0)*/
             };
 
         public override string Name => "Procedurally Generate Store Contents";
 
         public static List<BattleTechResourceType> BattleTechStoreResourceTypes => new List<BattleTechResourceType> { BattleTechResourceType.AmmunitionBoxDef, BattleTechResourceType.UpgradeDef, BattleTechResourceType.HeatSinkDef, BattleTechResourceType.JumpJetDef, BattleTechResourceType.WeaponDef };
 
+        public static bool PrefixInitializeShop(Shop __instance, ref List<string> collections)
+        {
+            Logger.Debug($"Noping out of Shop::Initialize.");
+            collections = null;
+            Logger.Debug($"Noped out of Shop::Initialize.");
+            return true;
+        }
+
         public static bool PrefixRefreshShop(Shop __instance, SimGameState ___Sim, StarSystem ___system)
         {
+            if (Myself._lastGenDateTime != null)
+            {
+                var difference = DateTime.Now - Myself._lastGenDateTime;
+                if (difference.TotalMinutes < 1)
+                {
+                    Logger.Debug($"Shop refresh request < 1 minute ago, skipping...");
+                    return false;
+                }
+            }
+
+            Myself._lastGenDateTime = DateTime.Now;
+
             ModFeatureBase<ProcGenStoreContentFeatureSettings>.Logger.Debug("Injecting custom shop inventory...");
             __instance.Clear();
 
@@ -60,6 +91,8 @@ namespace vfBattleTechMod_ProcGenStores.Mod.Features.ProcGenStoresContent
             var storeItems = ProcGenStoreContentFeature.Myself.StoreItemService.GenerateItemsForStore(shopType, ___system.Name, owningFaction.Name, currentDate, planetTagModifiers, ProcGenStoreContentFeature.Myself.Settings);
             var shopDefItems = storeItems.Select(item => { return new ShopDefItem(item.Id, ProcGenStoreContentFeature.Myself.dictResourceTypeToShopitemType[item.Type], 0, item.Quantity, item.Quantity == -1, false, 0); }).ToList();
 
+            Logger.Debug($"ShopDefItems = [\r\n{JsonConvert.SerializeObject(shopDefItems, Formatting.Indented)}]");
+
             var result = new ItemCollectionResult
             {
                 GUID = ___Sim.GenerateSimGameUID(),
@@ -69,6 +102,7 @@ namespace vfBattleTechMod_ProcGenStores.Mod.Features.ProcGenStoresContent
                 parentGUID = null,
                 pendingCount = 0
             };
+
             ___Sim.ItemCollectionResultGen.InsertShopDefItem(result.items, __instance.ActiveInventory);
             ModFeatureBase<ProcGenStoreContentFeatureSettings>.Logger.Debug("Injecting custom shop inventory complete.");
             return false;
