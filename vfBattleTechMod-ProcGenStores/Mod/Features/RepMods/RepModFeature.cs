4e075ebba0e7e3cc7c1f66613437d935141d512a
vengefire <vengefire@gmail.com>
2020-02-28 17:03:15 +0800

    Pluck about

--- vfBattleTechMod-ProcGenStores/Mod/Features/RepMods/RepModFeature.cs
+++ vfBattleTechMod-ProcGenStores/Mod/Features/RepMods/RepModFeature.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using BattleTech;
 using BattleTech.Framework;
 using Harmony;
@@ -16,6 +17,43 @@ namespace vfBattleTechMod_ProcGenStores.Mod.Features.RepMods
         {
             RepModFeature.Myself = this;
         }
+
+        /*
+        [HarmonyPatch(typeof(Mech), "Init")]
+        public static class Mech_Init_Patch
+        {
+            public static void Postfix(Mech __instance)
+            {
+                if (__instance.Combat.ActiveContract.ContractTypeValue.UsesFury || __instance.team.PlayerControlsTeam)
+                    return;
+
+                // Build up a list of Mech Components that we can dynamically upgrade... weapons and misc upgrades
+                var mechComponentsToUpgrade = __instance.Weapons.Cast<MechComponent>().ToList();
+                mechComponentsToUpgrade.AddRange(__instance.miscComponents.ToList());
+
+                foreach (var component in mechComponentsToUpgrade)
+                {
+                    // Get the MechComponentDef from the object
+                    var definition = component.mechComponentRef.Def;
+                    
+                    // Exclude if it's not stock
+                    if (!definition.ComponentTags.Contains("component_type_stock"))
+                    {
+                        continue;
+                    }
+                    
+                    Logger.Log("Old Component: " + definition.Description.Id);
+                    // Upgrade the component based on it's derived type
+                    MechComponentDef replacementDefinition = component is Weapon ? UpgradeWeapons(__instance.Combat.ActiveContract, definition) : UpgradeUpgrades(__instance.Combat.ActiveContract, definition);
+                    // Set the ComponentDefId on the component. This forces a Reload of the component from the Data Manager...
+                    component.baseComponentRef.ComponentDefID = replacementWeaponDefinition.Description.Id;
+                    component.mechComponentRef.ComponentDefID = replacementWeaponDefinition.Description.Id;
+                    // Update the protected Def property to the refreshed component def value to keep everything nice and consistent...
+                    Traverse.Create(component).Property("componentDef").SetValue(component.baseComponentRef.Def);
+                    Logger.Log("New Component: " + component.mechComponentRef.Def.Description.Id);
+                }
+            }
+        }*/
 
         public static List<IModPatchDirective> GetPatchDirectives =>
             new List<IModPatchDirective>
